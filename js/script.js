e91e57dbb751cdc58b018113d25bb2e119225f9a
zack32527 <157523220+zack32527@users.noreply.github.com>
2024-04-24 21:26:29 -0400

    Finished Videos

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ window.onload = function () {
     document.getElementById("copyright").innerHTML = new Date().getFullYear();
     
 }
+
+
+   // When the user scrolls down 800px from the top of the document, show the button
+   window.onscroll = function() {scrollFunction()};
+
+   function scrollFunction() {
+     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+       document.getElementById("cornerBtn").style.display = "block";
+     } else {
+       document.getElementById("cornerBtn").style.display = "none";
+     }
+   }
+
+   // When the user clicks on the button, scroll to the top of the document
+   function topFunction() {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
+   }
